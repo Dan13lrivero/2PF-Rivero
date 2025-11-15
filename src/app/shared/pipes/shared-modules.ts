932a3a398c85487cb84c80f66278987782e5cd81
Jorge Daniel <jorgedanielriveroc@gmail.com>
2025-11-15 10:20:19 -0300

    eliminacion de pipes no utilizados, limpieza y organizacion del codigo

--- src/app/shared/pipes/shared-modules.ts
+++ src/app/shared/pipes/shared-modules.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CapitalizePipe } from './capitalize/capitalize-pipe';
-import { Resaltado } from '../directives/resaltado';
 import { NombreCompletoPipe } from '../pipes/nombreCompleto/nombreCompleto-pipe';
 
 
 
 @NgModule({
   declarations: [
-    CapitalizePipe,
-    Resaltado,
     NombreCompletoPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CapitalizePipe, Resaltado, NombreCompletoPipe
+    NombreCompletoPipe
   ]
 })
 export class SharedModule { }
